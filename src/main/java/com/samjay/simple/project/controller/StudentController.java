472291f57314134d50ea6781e7a75137fa34a6c4
samjayworldwide <samuel.mbanisi@decagon.dev>
2023-11-09 23:47:40 +0100

    edited

--- src/main/java/com/samjay/simple/project/controller/StudentController.java
+++ src/main/java/com/samjay/simple/project/controller/StudentController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String welcomePage(){
         return "welcome customer";
     }
